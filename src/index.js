41225a8cdc2002cc4a778a9d93fd332e2c3e4c5f
cbkewagamang <cbkewagamang@gmail.com>
2025-10-28 17:31:22 +0200

    added API integration

--- src/index.js
+++ src/index.js
@@ -1,24 +1,30 @@
-function generatePoem(event) {
-  event.preventDefault();
-  //alert("Generating poem");
-
-  let poemElement = document.querySelector("#poem");
+function displayPoem(response) {
+  console.log("poem generated");
 
   new Typewriter("#poem", {
-    strings: `Roses are red,
-Violets are blue,
-My heart finds its rhythm,
-When thinking of you.
-
-The moonlight may whisper,
-Of love pure and true,
-But no words could capture,
-What I feel for you..🌹
-`,
+    strings: response.data.answer,
     autoStart: true,
     delay: 50,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+  //alert("Generating poem");
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "tb344ao2f3185a0fbb6a9c476d08743d";
+  let context =
+    "You are a romantic poem expert and love to write short poems. your mission is to write 5 line poem. sign at the end with <strong>SheCodes AI</strong> Make sure to follow user instructions. Do not start every line with 'In twilight's glow'";
+  let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("generating poem");
+  console.log(`prompt: ${prompt}`);
+  console.log(`context:${context}`);
+  axios.get(apiUrl).then(displayPoem);
+  //let poemElement = document.querySelector("#poem");
 }
 
 let formElement = document.querySelector("#poem-generator-form");
